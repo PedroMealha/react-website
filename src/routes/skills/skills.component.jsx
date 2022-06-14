33141ba90f55b8fec7191f4ff9f22cb276faae6a
pedromealhamb <pedro.mealha@mainbrain.pt>
2022-06-14 16:14:43 +0100

    CV added for download

--- src/routes/skills/skills.component.jsx
+++ src/routes/skills/skills.component.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 import './skills.styles.scss';
 
 const Skills = () =>
-	<div>..soon</div>
+	<div className="skills">
+		..soon<br />
+		<Link to="%PUBLIC_URL%/CV_Pedro_Mealha.pdf" target="_blank" download>Download CV</Link>
+	</div>
 
 export default Skills;
