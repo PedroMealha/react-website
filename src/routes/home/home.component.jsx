8bf2c62fff1866d503a9c2924082108e014ab045
pedromealhamb <pedro.mealha@mainbrain.pt>
2022-06-15 13:43:03 +0100

    responsive and minor fixes

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,9 +2,10 @@ import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import Header from '../../components/header/header.component';
-
 import StackCard from "../../components/stack-card/stack-card.component";
 import Progress from "../../components/progress/progress.component";
+
+import Logo from '../../assets/logo.svg';
 
 import './home.styles.scss';
 
@@ -71,7 +72,9 @@ const Home = () => {
 		<Fragment>
 			<Header section={ 'profile' } />
 			<div className="home">
-				<p className="intro">Frontend Developer with <span>6+ years</span> of experience creating desktop & mobile web content with high focus on clean code, cross-browser compatibility, performance and asset management using <span>Javascript</span>, <span>HTML</span> and <span>CSS</span> (Less/Sass).<br /><br />
+				<img className="logo" src={ Logo } alt="logo" />
+
+				<p className="intro">Frontend Developer with <span>6+ years</span> of experience creating desktop & mobile web content with high focus on clean code, cross-browser compatibility, performance and asset management as well as technical expertise on <span>interactive</span>, <span>dynamic</span> and <span>complex creatives</span> using <span>Javascript</span>, <span>HTML</span> and <span>CSS</span> (Less/Sass).<br /><br />
 					I am a person who always strives to achieve the <span>highest possible standard</span> for anything that crosses my path and, having a <span>Design</span> background, I implement that knowledge on outputting work that won’t be just <span>functional</span> but also <span>appealing</span> to the end user.</p>
 
 				<div className="strengths">
